de1535a0fe129dfe1b34cd153be63bcc6ff75c0e
Syed Faraaz Ahmad <faraaz98@live.com>
2018-10-11 21:19:14 +0530

    make login page frontend

--- app/routes/users.js
+++ app/routes/users.js
@@ -6,4 +6,12 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.get('/new', (req, res, next) => {
+  res.render('signup');
+});
+
+router.get('/session/new', (req, res, next) => {
+  res.render('login');
+});
+
 module.exports = router;
